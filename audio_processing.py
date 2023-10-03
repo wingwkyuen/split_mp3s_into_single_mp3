5c4299aa3f422daf6979b031c36946504a646708
Wing K.Y <strongkill@yahoo.com.hk>
2023-10-03 16:31:35 +0800

    Update audio_processing.py

--- audio_processing.py
+++ audio_processing.py
@@ -2,7 +2,7 @@ import json
 from pydub import AudioSegment
 
 
-file_path = "play_list_file.txt"  # 替换为实际文件路径
+file_path = "play_list_file.txt" 
 
 jsondata = []
 playlist = []
@@ -35,7 +35,6 @@ for line in lines:
             jsondata.append({"start": start_time, "end": "", "title": title})
             jsondata[len(jsondata)-2]["end"] = start_time
 
-# 将最后一个 JSON 对象的结束时间赋值为播放列表文件中的最后一个结束时间
 if jsondata:
     jsondata[-1]["end"] = end_time
 #print(jsondata[0])
@@ -43,7 +42,6 @@ if jsondata:
 
 audio = AudioSegment.from_file("long_audio.mp3", format="mp3")
 
-# 将音频文件按照播放列表分割
 for i in range(0,len(jsondata)):
     print(jsondata[i])
     start_time = jsondata[i]["start"]
@@ -53,8 +51,7 @@ for i in range(0,len(jsondata)):
     start = (int(start_time.split(":")[0]) * 3600 + int(start_time.split(":")[1]) * 60 + int(start_time.split(":")[2])) * 1000
     end = (int(start_time.split(":")[0]) * 3600 + int(end_time.split(":")[1]) * 60 + int(end_time.split(":")[2])) * 1000
 
-    # 分割音频文件
     track_audio = audio[start:end]
     #print(f"/app/output/Track_{i+1}_{title}.mp3")
-    # 保存分割后的音频文件
+
     track_audio.export(f"/app/output/Track_{i+1}_{title}.mp3", format="mp3", tags={'artist': title , 'album': album, 'comments': comments})
